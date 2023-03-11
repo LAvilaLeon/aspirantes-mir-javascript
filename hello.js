c2970345a4827377c1b3beef705ef9047765f55c
LuAvilaLeon <123138837+LAvilaLeon@users.noreply.github.com>
2023-03-11 00:23:26 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+function hola(cadenaTexto) {
+    let salida= "Hola "+ cadenaTexto + "!"
+    return salida;
+}
+
+console.log(hola("Pedro")); // "Hola Pedro!"
+console.log(hola("Juan")); // "Hola Juan!"
+console.log(hola(" ")); // "Hola !"
