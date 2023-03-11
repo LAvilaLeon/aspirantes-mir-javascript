75bcc01979c3fd739628a789d88e52274070a972
LuAvilaLeon <123138837+LAvilaLeon@users.noreply.github.com>
2023-03-11 00:06:52 -0500

    solucion a los ejercicios de js

---
+++ js-1/sum.js
@@ -0,0 +1,12 @@
+function suma(positivo){
+    return positivo*(positivo+1)/2
+}
+
+n=4
+console.log(suma(n)) // 1 + 2 + 3 + 4 = 10
+
+n=10
+console.log(suma(n)) // 55
+
+n=15
+console.log(suma(n)) // 120
